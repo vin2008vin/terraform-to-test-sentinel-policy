43e476ff2ee6c2958e1e6ddad902b302cb392116
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-03 15:18:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 
