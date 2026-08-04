21cebe5ecc4eee06a8f9dd51005dec3994b7d2a8
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-04 17:41:32 +0530

    Update AWS provider region and instance type

--- main.tf
+++ main.tf
@@ -1,9 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Web server security group"
+
+  tags = {
+    Name  = "Web"
+    Owner = "web-team"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.web.id
+
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  to_port     = 80
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-  instance_type = "t3.large"
+  instance_type = "t2.small"
 
 }
