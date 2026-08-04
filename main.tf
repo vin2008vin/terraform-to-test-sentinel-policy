b23e85aa0144d680dde17750d20ceadece34b873
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-04 06:32:21 +0530

    Change instance type from t2.small to t3.large

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-  instance_type = "t2.small"
+  instance_type = "t3.large"
 
   tags = {
     Environment = "dev"   # Mandatory tag
