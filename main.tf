8b24b91a2046b7eabd3e319d96be0e90fa6343ca
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-04 05:59:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-  instance_type = "t3.micro"
+  instance_type = "t2.small"
 
   tags = {
     Environment = "dev"   # Mandatory tag
