62832d22439e40c02a473041998c5bd6f217f9a9
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-04 07:03:57 +0530

    Remove 'Environment' tag from AWS instance resource
    Removed mandatory tag 'Environment' from AWS instance.

--- main.tf
+++ main.tf
@@ -6,7 +6,4 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
   instance_type = "t3.large"
 
-  tags = {
-    Environment = "dev"   # Mandatory tag
-  }
 }
