5a476ec4e489e8c7177d42d93589e5e0233d9b43
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-07 19:36:50 +0530

    Update Terraform organization and workspace name

---
+++ versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.26.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+
+  backend "remote" {
+    organization = "vinay-tech"
+
+    workspaces {
+      name = "staging-k8s"
+    }
+  }
+
+}
