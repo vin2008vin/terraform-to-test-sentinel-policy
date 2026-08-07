734ab6691474bc9f24d8263dee8466ce710ce7cb
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-08-07 18:54:14 +0530

    Change instance type for AWS instance
    Updated the instance type for the AWS instance resource.

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-  instance_type = "c5a.large"
+
 
 }
 
